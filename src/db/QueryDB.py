45b9cb8e49f62e0faac1ce47bb3da6562cba33e8
Alf Alexander <alf1337@gmail.com>
2016-04-19 16:11:36 +0200

    Clean up QueryDB and make it work

--- src/db/QueryDB.py
+++ src/db/QueryDB.py
@@ -6,66 +6,39 @@ import MySQLdb
 import datetime
 
 class QueryDB:
-	#Constructor, getting the db connection
-	def __init__(self, db):
-		self.cursor = db.cursor()
-		self.trip_id = 0
-                self._db = db
-                self._last_time = None
-	
-	#Finding the last trip id in database data table
-	def find_trip_id(self):
-		self.cursor.execute("SELECT MAX(tripId) FROM `skyclouds_blacktap`.`data`")
-		self.trip_id = self.cursor.fetchone()[0]
-                if self.trip_id is None:
-                    self.trip_id = 1
-                else:
-                    self.trip_id += 1
-                print self.trip_id
-	
-	#Loop the result list, send query according to result name
-	def query(self, sortedResults):
-		timestamp = datetime.datetime.fromtimestamp(int(sortedResults[0])).strftime('%Y-%m-%d %H:%M:%S')
-                if self._last_time == timestamp:
-                    return
-                self._last_time = timestamp
-		
-		#Updating trip id
-		
-		query = ("INSERT INTO `skyclouds_blacktap`.`data` (" +\
-														"`bil_idBil`," +\
-														"`timestamp`," +\
-														"`tripId`," +\
-														"`steering_wheel_angle`," +\
-														"`torque_at_transmission`," +\
-														"`engine_speed`," +\
-														"`vehicle_speed`," +\
-														"`accelerator_pedal_position`," +\
-														"`parking_brake_status`," +\
-														"`brake_pedal_status`," +\
-														"`transmission_gear_position`," +\
-														"`gear_lever_position`," +\
-														"`headlamp_status`," +\
-														"`odometer`," +\
-														"`ignition_status`," +\
-														"`fuel_level`," +\
-														"`fuel_consumed_since_restart`," +\
-														"`door_status`," +\
-														"`high_beam_status`," +\
-														"`windshield_wiper_status`," +\
-														"`latitude`," +\
-														"`longitude`" +\
-														") VALUES (" + "'1',")
-		query = query + "'" + timestamp + "'," + str(self.trip_id) + ","
-		
-		for index in range(1, len(sortedResults)):
-			if len(sortedResults[index]) > 0:
-				query = query + str(sortedResults[index][0])
-			else:
-				query = query + "0"
-			if index != 19:
-				query = query + ","
-                        print index
-		query = query + ");"
-		self.cursor.execute(query)
-                self._db.commit()
+    #Constructor, getting the db connection
+    def __init__(self, db):
+        self.cursor = db.cursor()
+        self.trip_id = 0
+        self._db = db
+        self._last_time = None
+
+  #Finding the last trip id in database data table
+    def find_trip_id(self):
+        self.cursor.execute("SELECT MAX(tripId) FROM `skyclouds_blacktap`.`data`")
+        self.trip_id = self.cursor.fetchone()[0]
+
+        if self.trip_id is None:
+            self.trip_id = 1
+        else:
+            self.trip_id += 1
+            print self.trip_id
+
+  #Loop the result list, send query according to result name
+    def query(self, d):
+        timestamp = datetime.datetime.fromtimestamp(int(d.get('timestamp'))).strftime('%Y-%m-%d %H:%M:%S')
+        if self._last_time == timestamp:
+            return
+        d['timestamp'] = timestamp
+        print(timestamp)
+
+        #Updating trip id
+        query = "INSERT INTO skyclouds_blacktap.data (bil_idBil, tripId, "
+        query += ', '.join(d.keys())
+        query += ") VALUES ( '2', %s, " % self.trip_id
+        query += ', '.join("'%s'" % d[key] for key in d.keys())
+        query += ");"
+        print(query)
+
+        self.cursor.execute(query)
+        self._db.commit()
